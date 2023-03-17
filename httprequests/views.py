5b8692366954b1686389d0f7d824cbb3269f586e
Isobel <izzybourne261@gmail.com>
2023-03-17 15:50:21 +0000

    Added html page

--- httprequests/views.py
+++ httprequests/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'httprequests/home.html')
+
+def hello(request):
+    return HttpResponse('<h1 style="color:blue;">hello lalalalala</h1>')
