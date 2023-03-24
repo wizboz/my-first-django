0b08e79692bb5036f0cf20115e87c8df0320cf54
Isobel <izzybourne261@gmail.com>
2023-03-24 14:50:39 +0000

    added inputs and buttons with new page

--- httprequests/views.py
+++ httprequests/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-
+class Avatar:
+    def __init__(self, number, name):
+        self.number = number
+        self.name = name
+    def __str__(self):
+        return self.name+"'s favourite number is "+self.number
+    
 def home(request):
     return render(request, 'httprequests/home.html')
 
 def hello(request):
-    return HttpResponse('<h1 style="color:blue;">hello lalalalala</h1>')
+    the_name = request.GET.get('name')
+    the_number = request.GET.get('number')
+    ava = Avatar(the_number, the_name)
+    return render(request, 'httprequests/hello.html', {'name':ava})
